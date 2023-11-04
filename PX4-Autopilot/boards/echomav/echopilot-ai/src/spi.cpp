e55ea8f7069d3f76e3109b7cd9327b44dc469fad
climr <stinsonbj@ornl.gov>
2023-11-04 14:17:55 -0400

    remove comments

--- PX4-Autopilot/boards/echomav/echopilot-ai/src/spi.cpp
+++ PX4-Autopilot/boards/echomav/echopilot-ai/src/spi.cpp
@@ -35,9 +35,6 @@
 #include <drivers/drv_sensor.h>
 #include <nuttx/spi/spi.h>
 
-//PF5 was not connected on R1 boards, fix is to use PC3 for the SPI_DEV flash device
-//Change back to PF5 on R1a and future
-
 constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 	initSPIBus(SPI::Bus::SPI1, {
 			initSPIDevice(DRV_IMU_DEVTYPE_ICM42688P, SPI::CS{GPIO::PortF, GPIO::Pin2}, SPI::DRDY{GPIO::PortB, GPIO::Pin4}),  
