ba0f8f92071fba605a091491c9c745770cf6810a
BradS <bstinson@echomav.com>
2024-06-13 09:27:39 -0400

    add orin default fan control

--- install_l4t_orin.sh
+++ install_l4t_orin.sh
@@ -2,6 +2,8 @@
 # Installation of dtb, cfg and extlinux.conf file for building L4T for Orin NX on the EchoPilot AI
 # Usage: ./install_l4t_orin.sh [path to Linux_for_Tegra]
 
+SUDO=$(test ${EUID} -ne 0 && which sudo)
+
 if [[ $# != 1 ]] ; then
   echo 'USAGE: ./install_l4t_orin.sh <Path to Linux_for_Tegra, e.g. ~/Orin/Linux_for_Tegra/>'
   exit 0
@@ -28,10 +30,12 @@ echo "Copying files..."
 cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts $INSTALL_PATH/bootloader/t186ref/BCT/.
 cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-scr-p3767-0000.dts $INSTALL_PATH/bootloader/t186ref/BCT/.
 cp Linux_for_Tegra/kernel/dtb/tegra234-p3767-0000-p3509-a02.dtb $INSTALL_PATH/kernel/dtb/.
+$SUDO cp Linux_for_Tegra/rootfs/etc/nvpower/nvfancontrol/nvfancontrol_p3767_0000.conf $INSTALL_PATH/rootfs/etc/nvpower/nvfancontrol/.
 
 
 echo ""
 echo "Success!!!"
 echo ""
 echo ""
-echo "Flash the Orin from the Linux_for_Tegra directory using the command: sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_external.xml -p "-c bootloader/t186ref/cfg/flash_t234_qspi.xml" --showlogs --network usb0 p3509-a02+p3767-0000 internal"
+echo "You may now flash the Orin from the Linux_for_Tegra directory using the command: sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_external.xml -p "-c bootloader/t186ref/cfg/flash_t234_qspi.xml" --showlogs --network usb0 p3509-a02+p3767-0000 internal"
+echo ""
