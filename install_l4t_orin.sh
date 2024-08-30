9332e3dc8fb4c69e54ec2d108f087837e8df11a8
BradS <bstinson@echomav.com>
2024-08-30 11:11:38 -0400

    update orin script to handle L4T 35 or 36

--- install_l4t_orin.sh
+++ install_l4t_orin.sh
@@ -23,19 +23,52 @@ if [[ "$INSTALL_PATH" != *"Linux_for_Tegra"* ]]; then
   exit 0
 fi
 
-echo "Using Install Path: " $INSTALL_PATH
+echo "Using Install Path:" $INSTALL_PATH
 
-echo "Copying files..."
-# copy files
-cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts $INSTALL_PATH/bootloader/t186ref/BCT/.
-cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-scr-p3767-0000.dts $INSTALL_PATH/bootloader/t186ref/BCT/.
-cp Linux_for_Tegra/kernel/dtb/tegra234-p3767-0000-p3509-a02.dtb $INSTALL_PATH/kernel/dtb/.
-$SUDO cp Linux_for_Tegra/rootfs/etc/nvpower/nvfancontrol/nvfancontrol_p3767_0000.conf $INSTALL_PATH/rootfs/etc/nvpower/nvfancontrol/.
+# check the BSP_BRANCH as different action is required for L4T 36+
+BSP_PATH="${INSTALL_PATH}/nv_tegra/bsp_version"
+
+echo "Looking for L4T Version at:" $BSP_PATH
+
+# Check if the file exists
+if [ ! -f "$BSP_PATH" ]; then
+  #not found, lets assume 35
+  BSP_BRANCH=35
+  echo "Warning, L4T Version not found, defaulting to Vesion:" $BSP_BRANCH
+else
+  BSP_BRANCH=$(grep "^BSP_BRANCH=" "$BSP_PATH" | awk -F= '{print $2}')
+  echo "Found L4T Version:" $BSP_BRANCH
+fi
+
+if [ "$BSP_BRANCH" -eq 36 ]; then    
+    echo "Copying files for L4T 36..."
+    # copy files
+    cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts $INSTALL_PATH/bootloader/generic/BCT/.
+    cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-scr-p3767-0000.dts $INSTALL_PATH/bootloader/generic/BCT/.
+    cp Linux_for_Tegra/kernel/dtb/tegra234-p3767-0000-p3509-a02.dtb $INSTALL_PATH/kernel/dtb/.
+    $SUDO cp Linux_for_Tegra/rootfs/etc/nvpower/nvfancontrol/nvfancontrol_p3767_0000.conf $INSTALL_PATH/rootfs/etc/nvpower/nvfancontrol/.    
+    echo "Success!!!"
+    echo ""
+    echo "You may now flash the Orin from the Linux_for_Tegra directory using the command:"    
+    echo 'sudo ./tools/kernel_flash/l4t_initrd_flash.sh -c ./tools/kernel_flash/flash_l4t_external.xml --external-device nvme0n1p1 -p "-c bootloader/generic/cfg/flash_t234_qspi.xml" p3509-a02-p3767-0000 internal'
+    echo ""
+elif [ "$BSP_BRANCH" -eq 35 ]; then     
+    echo "Copying files for L4T 35..."
+    # copy files
+    cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-misc-p3767-0000.dts $INSTALL_PATH/bootloader/t186ref/BCT/.
+    cp Linux_for_Tegra/bootloader/t186ref/BCT/tegra234-mb2-bct-scr-p3767-0000.dts $INSTALL_PATH/bootloader/t186ref/BCT/.
+    cp Linux_for_Tegra/kernel/dtb/tegra234-p3767-0000-p3509-a02.dtb $INSTALL_PATH/kernel/dtb/.
+    $SUDO cp Linux_for_Tegra/rootfs/etc/nvpower/nvfancontrol/nvfancontrol_p3767_0000.conf $INSTALL_PATH/rootfs/etc/nvpower/nvfancontrol/.    
+    echo "Success!!!"
+    echo ""
+    echo "You may now flash the Orin from the Linux_for_Tegra directory using the command:"
+    echo 'sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_external.xml -p "-c bootloader/t186ref/cfg/flash_t234_qspi.xml" --showlogs --network usb0 p3509-a02+p3767-0000 internal'
+    echo ""
+else
+    echo "Version ${BSP_BRANCH} of L4T detected is not supported. Please use L4T 35.x or 36.x"
+fi
 
 
-echo ""
-echo "Success!!!"
-echo ""
-echo ""
-echo "You may now flash the Orin from the Linux_for_Tegra directory using the command: sudo ./tools/kernel_flash/l4t_initrd_flash.sh --external-device nvme0n1p1 -c tools/kernel_flash/flash_l4t_external.xml -p "-c bootloader/t186ref/cfg/flash_t234_qspi.xml" --showlogs --network usb0 p3509-a02+p3767-0000 internal"
-echo ""
+
+
+
