9c02de4fbe81c13ea5005ed3bfae8b0f8cfbb2ef
Garrett Mesmer <190508517+gmesm@users.noreply.github.com>
2025-01-14 15:33:56 -0500

    patch nvidia flash scripts to allow --reuse-uuid to be passed through from l4t_initrd_flash.sh to flash.sh when flashing external (NVMe) and internal (qspi) in the same command

--- install_l4t_orin.sh
+++ install_l4t_orin.sh
@@ -62,6 +62,8 @@ if [ "$BSP_BRANCH" -eq 36 ]; then
     # add fix for disabled hdmi (corrected in 36.4+)
     apply_patch $INSTALL_PATH/.. "$SCRIPT_DIR"/patches/0001-fix-boot-with-missing-hdmi-on-orin-with-3rd-party-ca.patch
   fi
+  # allow --reuse-uuid to be passed through from l4t_initrd_flash.sh to flash.sh when flashing external (NVMe) and internal (qspi) in the same command
+  apply_patch $INSTALL_PATH/.. "$SCRIPT_DIR"/patches/0001-allow-reuse-uuid-to-be-passed-through-from-l4t_initr.patch
 
   # add fancontrol config
   $SUDO cp Linux_for_Tegra/rootfs/etc/nvpower/nvfancontrol/nvfancontrol_p3767_0000.conf $INSTALL_PATH/rootfs/etc/nvpower/nvfancontrol/. 
@@ -107,6 +109,8 @@ elif [ "$BSP_BRANCH" -eq 35 ]; then
   apply_patch $INSTALL_PATH/.. patches/0001-disable-board-eeprom-requirement-r35.patch
   # add fix for disabled hdmi (corrected in 36.4+)
   apply_patch $INSTALL_PATH/.. patches/0001-fix-boot-with-missing-hdmi-on-orin-with-3rd-party-ca-r35.patch
+  # allow --reuse-uuid to be passed through from l4t_initrd_flash.sh to flash.sh when flashing external (NVMe) and internal (qspi) in the same command
+  apply_patch $INSTALL_PATH/.. "$SCRIPT_DIR"/patches/0001-allow-reuse-uuid-to-be-passed-through-from-l4t_initr-r35.patch
 
   # add fancontrol config
   $SUDO cp Linux_for_Tegra/rootfs/etc/nvpower/nvfancontrol/nvfancontrol_p3767_0000.conf $INSTALL_PATH/rootfs/etc/nvpower/nvfancontrol/. 
